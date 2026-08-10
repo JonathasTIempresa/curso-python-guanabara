1f57db30a242dd32573f14958abb902b3fef146d
Admin Jonathas de Jesus <a.jonathas.j@a2.local>
2026-08-10 17:16:04 -0300

    Aula 4 finalizada

--- Aula4/scripts-Aula4/desafio1.py
+++ Aula4/scripts-Aula4/desafio1.py
@@ -1,3 +1,5 @@
+## Pegue o nome do cliente e mostre ma mensagem de boas vindas 
+
 nome = input ('Qual o seu nome? ') 
 
 print ('Seja bem vindo', nome +  ' a aula de número 4')
---
+++ Aula4/scripts-Aula4/desafio3.py
@@ -0,0 +1,13 @@
+###Soma dois numeros###
+
+numero1 = input ('Digite um número: ')
+numero2 = input ('Digite o segundo numero: ')
+
+
+
+
+
+soma = (numero1 + numero2)
+print (soma)
+print (numero1)
+print (numero2)
